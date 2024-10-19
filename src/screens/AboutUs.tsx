7cebd903663fb27f76024a23d473bef273e71ce2
Nikolay Dimitrov <n.zhdimitrov@gmail.com>
2024-10-19 11:04:12 +0300

    finished desktop aboutus screen

--- src/screens/AboutUs.tsx
+++ src/screens/AboutUs.tsx
@@ -1,4 +1,4 @@
-import { IconArrowRight } from "@tabler/icons-react";
+import { IconArrowRight, IconSend2 } from "@tabler/icons-react";
 import AboutImageMap from "../assets/AltAboutImage.jpg";
 import AboutImageLuggage from "../assets/AboutImage2.png";
 import AboutImageTraveller from "../assets/AboutImage3.jpg";
@@ -29,16 +29,60 @@ export const AboutUs = () => {
                     </button>
                 </form>
             </div>
-            <div className="bg-white w-[80%] mt-20 flex">
-                <div className="grid grid-cols-2">
+            <div className="bg-white w-[80%] mt-20 lg:flex">
+                <div className="grid grid-cols-2 -mr-20">
                     <div className="row-span-2">
                         <img src={AboutImageMap} className="border-4 border-white shadow-xl rounded-full w-[350px] h-[514px]" />
                     </div>
                     <img src={AboutImageLuggage} className="w-[160px] h-[207px]" />
                     <img src={AboutImageTraveller} className="border-4 border-white shadow-xl rounded-full w-[160px] h-[255px]" />
                 </div>
-                <div>
-
+                <div className="flex flex-col items-start">
+                    <h3 className="font-Grypen text-3xl text-primary-100">
+                        Get About Us
+                    </h3>
+                    <h2 className="font-SpaceGrotesk font-bold text-4xl leading-tight mt-2">
+                        More About Travelsy
+                    </h2>
+                    <p className="text-sm text-gray-300 mt-6">
+                        Amet cursus sit amet dictum. At erat pellentesque adipiscing commodo.<br/>
+                        Sit amet volutpat consequat mauris nunc congue nisi vitae suscipit. In<br/>
+                        arcu cursus euismod quis viverra nibh cras pulvinar. Vulputate eu sceler<br/>
+                        felis imperdiet proin fermentum. Arcu vitae elementum curabitur vitae<br/>
+                        nunc sed velit.
+                    </p>
+                    <p className="text-sm mt-2 text-gray-300">
+                        Rutrum quisque non tellus orci ac. Varius vel pharetra vel turpis nunc<br/>
+                        eget lorem dolor sed. Interdum posuere lorem ipsum dolor sit. Egestas<br/>
+                        maecenas pharetra convallis posuere morbi.
+                    </p>
+                    <div className="space-y-2 mt-6">
+                        <div className="flex items-center justify-start gap-2">
+                            <IconSend2 className="text-primary-100"/>
+                            <span className="font-semibold">
+                                Donec adipiscing tristique risus nec feugiat in
+                            </span>
+                        </div>
+                        <div className="flex items-center justify-start gap-2">
+                            <IconSend2 className="text-primary-100"/>
+                            <span className="font-semibold">
+                                Donec adipiscing tristique risus nec feugiat in
+                            </span>
+                        </div>
+                        <div className="flex items-center justify-start gap-2">
+                            <IconSend2 className="text-primary-100"/>
+                            <span className="font-semibold">
+                                Donec adipiscing tristique risus nec feugiat in
+                            </span>
+                        </div>
+                    </div>
+                    <button
+                        type="submit"
+                        className="mt-10 flex bg-primary-100 text-white gap-2 items-center px-6 py-3 rounded-full
+                        font-Mulish font-extrabold hover:brightness-105"
+                    >
+                        Read More <IconArrowRight/>
+                    </button>
                 </div>
             </div>
         </section>
